4e594978f53ad6168bf349c63de9cb09ad359761
zuhalal <zuhalalimulhadi19@gmail.com>
2022-08-03 21:00:28 +0700

    Add list recycler view

---
+++ app/src/main/java/com/zuhal/musicapp/ListMusicAdapter.kt
@@ -0,0 +1,51 @@
+package com.zuhal.musicapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+class ListMusicAdapter(private val listMusic: ArrayList<Music>):
+    RecyclerView.Adapter<ListMusicAdapter.ListViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Music)
+    }
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvArtist: TextView = itemView.findViewById(R.id.tv_artist)
+        var tvSongTitle: TextView = itemView.findViewById(R.id.tv_song_title)
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_music, parent,false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val music = listMusic[position]
+
+        Glide.with(holder.itemView.context)
+            .load(music.images)
+            .apply(RequestOptions().override(55,55))
+            .into(holder.imgPhoto)
+
+        holder.tvArtist.text = music.artists
+        holder.tvSongTitle.text = music.title
+//        holder.itemView.setOnClickListener({onItemClickCallback.onItemClicked(listMusic[holder.adapterPosition])})
+    }
+
+    override fun getItemCount(): Int {
+        return listMusic.size
+    }
+}
--- app/src/main/java/com/zuhal/musicapp/MainActivity.kt
+++ app/src/main/java/com/zuhal/musicapp/MainActivity.kt
@@ -3,12 +3,26 @@ package com.zuhal.musicapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvMusic: RecyclerView
+    private var list: ArrayList<Music> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("a", MusicData.listData.joinToString())
+        rvMusic = findViewById(R.id.rv_music)
+        rvMusic.setHasFixedSize(true)
+
+        list.addAll(MusicData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvMusic.layoutManager = LinearLayoutManager(this)
+        val listMusicAdapter = ListMusicAdapter(list)
+        rvMusic.adapter = listMusicAdapter
     }
 }
