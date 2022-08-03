2307e22596af0e17a3a272b3ccf97111b9fbe8e1
zuhalal <zuhalalimulhadi19@gmail.com>
2022-08-03 20:37:36 +0700

    Initial commit

---
+++ app/src/main/java/com/zuhal/musicapp/Music.kt
@@ -0,0 +1,20 @@
+package com.zuhal.musicapp
+
+data class Music (
+    var track_id: String = "",
+    var title: String = "",
+    var artists: String = "",
+    var artist_image: String = "",
+    var album: String = "",
+    var album_id: String = "",
+    var duration: String = "",
+    var genres: String = "",
+    var language: String = "",
+    var label: String = "",
+    var release_date: String = "",
+    var play_count: String = "",
+    var favorite_count: String = "",
+    var song_url: String = "",
+    var album_url: String = "",
+    var images: String = ""
+)
